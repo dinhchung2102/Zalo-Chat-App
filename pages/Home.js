5741e85dd18f5be71b975a4fc62380b2831cd22d
dinhchung2102 <vodinhchung4@gmail.com>
2025-01-26 09:54:05 +0700

    add intro swiper

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,13 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LanguageSelect from "../components/selects/LanguageSelect";
-import { BANNER_HEIGHT, BANNER_WIDTH } from "../constants/bannerSize";
 import { BASE_UNIT } from "../constants/screen";
-import { textLargeSize, textMediumSize } from "../constants/fontSize";
 import LargeButton from "../components/buttons/LargeButton";
 import { useRecoilState, useRecoilValue } from "recoil";
 import SelectLanguageModal from "../components/modals/SelectLanguageModal";
 import { languageState, modalLanguageState } from "../state/PrimaryState";
+import IntroSwiper from "../components/swipers/IntroSwiper";
 
 export default function Home() {
   const selectedLanguage = useRecoilValue(languageState);
@@ -19,15 +18,7 @@ export default function Home() {
         <LanguageSelect onPress={() => setModalVisible(true)} />
       </View>
       <View style={styles.content}>
-        <Text style={styles.textAppName}>Zalo</Text>
-        <View style={styles.swiper}>
-          <Image
-            source={require("../assets/home/video-on-dinh.png")}
-            style={{ width: BANNER_WIDTH, height: BANNER_HEIGHT }}
-          />
-          <Text style={{fontSize: textMediumSize, fontWeight:'bold', textAlign:'center', marginBottom: BASE_UNIT*0.01}}>{selectedLanguage === 'vie' ? 'Gọi video ổn định' : 'Smooth video call'}</Text>
-          <Text style={{fontSize: textMediumSize - 1, color:'#c4c4c4', textAlign:'center'}}>{selectedLanguage === 'vie' ? 'Trò chuyện thật đã với chất lượng video ổn định mọi lúc, mọi nơi' : 'Perform video calls with high-quality over all type of networks'}</Text>
-        </View>
+        <IntroSwiper />
       </View>
       <View style={styles.footer}>
         <LargeButton
@@ -64,17 +55,12 @@ const styles = StyleSheet.create({
   content: {
     alignItems: "center",
     marginTop: BASE_UNIT * 0.02,
-  },
-  textAppName: {
-    fontSize: textLargeSize,
-    fontWeight: "700",
-    marginBottom: BASE_UNIT * 0.3,
-    color: "#006DFE",
+    flex: 1,
   },
   footer: {
     alignItems: "center",
     justifyContent: "flex-end",
-    flex: 1,
+    flex: 0.3,
     paddingBottom: BASE_UNIT * 0.07,
   },
 });
