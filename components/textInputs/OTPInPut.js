1238171ac1cada744f83c43a36aab206f136edcf
dinhchung2102 <vodinhchung4@gmail.com>
2025-01-27 13:35:31 +0700

    fix otp input styles

--- components/textInputs/OTPInPut.js
+++ components/textInputs/OTPInPut.js
@@ -2,16 +2,18 @@ import React, { useState, useRef } from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { BASE_UNIT } from "../../constants/screen";
 import { textMediumPlus } from "../../constants/fontSize";
+import { Colors } from "../../styles/Colors";
 
-export default function OTPInPut({otp, setOtp, onSubmit}) {
-  
+export default function OTPInPut({ otp, setOtp, onSubmit }) {
+  const [focusedIndex, setFocusedIndex] = useState(null);
+
   const inputRefs = [
     useRef(null),
     useRef(null),
     useRef(null),
     useRef(null),
     useRef(null),
-    useRef(null)
+    useRef(null),
   ];
 
   const handleOtpChange = (text, index) => {
@@ -22,6 +24,18 @@ export default function OTPInPut({otp, setOtp, onSubmit}) {
     if (text.length === 1 && index < 5) {
       inputRefs[index + 1].current.focus();
     }
+
+    if (text.length === 0 && index > 0) {
+      inputRefs[index - 1].current.focus();
+    }
+  };
+
+  const handleFocus = (index) => {
+    setFocusedIndex(index);
+  };
+
+  const handleBlur = () => {
+    setFocusedIndex(null);
   };
 
   const handleSubmit = () => {
@@ -35,11 +49,16 @@ export default function OTPInPut({otp, setOtp, onSubmit}) {
         <TextInput
           key={index}
           ref={inputRefs[index]}
-          style={styles.input}
+          style={[
+            styles.input,
+            focusedIndex === index && { borderColor: Colors.primary },
+          ]}
           maxLength={1}
           keyboardType="numeric"
           value={digit}
           onChangeText={(text) => handleOtpChange(text, index)}
+          onFocus={() => handleFocus(index)}
+          onBlur={handleBlur}
         />
       ))}
     </View>
@@ -48,20 +67,20 @@ export default function OTPInPut({otp, setOtp, onSubmit}) {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-    paddingHorizontal: BASE_UNIT*0.1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: BASE_UNIT * 0.1,
   },
   input: {
-    width: BASE_UNIT*0.1,
-    height: BASE_UNIT*0.12,
+    width: BASE_UNIT * 0.1,
+    height: BASE_UNIT * 0.12,
     borderColor: "#ccc",
     borderWidth: 1,
     textAlign: "center",
     fontSize: textMediumPlus,
-    fontWeight:'500',
-    borderRadius: BASE_UNIT*0.02,
+    fontWeight: "500",
+    borderRadius: BASE_UNIT * 0.02,
   },
 });
