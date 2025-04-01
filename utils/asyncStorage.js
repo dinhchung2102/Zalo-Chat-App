eabcb767ac4ce8c8075b6a5c266c89a5000b602a
dinhchung2102 <vodinhchung4@gmail.com>
2025-04-01 11:15:42 +0700

    sign up & login

--- utils/asyncStorage.js
+++ utils/asyncStorage.js
@@ -27,3 +27,34 @@ export const removeTempToken = async () => {
     console.error("Lỗi khi xóa token:", error);
   }
 };
+
+
+export const saveLoginResult = async (loginResult) => {
+  try {
+    await AsyncStorage.setItem("loginResult", JSON.stringify(loginResult)); 
+    console.log("Lưu thông tin đăng nhập:", loginResult);
+  } catch (error) {
+    console.error("Lỗi khi lưu thông tin đăng nhập:", error);
+  }
+};
+
+// Lấy thông tin đăng nhập
+export const getLoginResult = async () => {
+  try {
+    const loginResult = await AsyncStorage.getItem("loginResult");
+    return loginResult ? JSON.parse(loginResult) : null; 
+  } catch (error) {
+    console.error("Lỗi khi lấy thông tin đăng nhập:", error);
+    return null;
+  }
+};
+
+// Xóa thông tin đăng nhập
+export const removeLoginResult = async () => {
+  try {
+    await AsyncStorage.removeItem("loginResult");
+    console.log("Thông tin đăng nhập đã bị xóa khỏi AsyncStorage");
+  } catch (error) {
+    console.error("Lỗi khi xóa thông tin đăng nhập:", error);
+  }
+};
